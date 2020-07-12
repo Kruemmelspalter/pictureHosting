ae431f184aad89b847164d43d8bb900980af6eb9
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-07-12 07:19:36 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import flask, os
+app = flask.Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'static'
+
+from werkzeug import secure_filename
+
+@app.route('/')
+@app.route('/index')
+@app.route('/upload', methods=['GET', 'POST'])
+def index():
+    if flask.request.method == 'GET':
+        return flask.render_template('upload.html')
+    elif flask.request.method == 'POST':
+        f = flask.request.files['file']
+        fname = secure_filename((flask.request.form.get('fname') if flask.request.form.get('fname')!="" else f.filename))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+        return 'File uploaded successful <a href="static/'+fname+'">here</a>'
+
+
+if __name__ == "__main__":
+    app.run()
